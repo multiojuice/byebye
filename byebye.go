cdae951dd2e3e747ff076a0d3382fc4b6181bf18
Owen J Sullivan <multiojuice@gmail.com>
2018-10-12 20:01:53 -0700

    Starting to read in config file

--- byebye.go
+++ byebye.go
@@ -1,13 +1,40 @@
 package main
 
 import (
-	"log"
-	"os/exec"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	cmd := exec.Command("pkill", "tmux")
-	log.Printf("Bye bye!")
-	err := cmd.Run()
-	log.Printf("Command finished with error: %v", err)
+	f, err := os.Open("/home/multiojuice/.byebyerc")
+	check(err)
+
+	configReader := bufio.NewReader(f)
+
+	line, isPrefix, readErr := configReader.ReadLine()
+	check(readErr)
+
+	for line != nil {
+		fmt.Printf("%s\n", string(line))
+		line, isPrefix, readErr = configReader.ReadLine()
+
+		if isPrefix || readErr == io.EOF {
+			break
+		}
+	}
+
+	// cmd := exec.Command("pkill", "-SIGINT", "tmux")
+	// log.Printf("Bye bye!")
+	// cmdErr := cmd.Run()
+	// log.Printf("Command finished with error: %v", cmdErr)
+
+	f.Close()
 }
